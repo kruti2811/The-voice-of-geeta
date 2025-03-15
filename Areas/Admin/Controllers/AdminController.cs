e20954a7539091272dd8756628bd526dba064e0b
Er-KrutiPatel <patelkrutisubhash@gmail.com>
2025-03-15 23:19:01 +0530

    Admin Pannel by Kruti

---
+++ Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace The_voice_of_geeta.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dashborad()
+        {
+            return View();
+        }public IActionResult ActiveUsers()
+        {
+            return View();
+        }
+        public IActionResult DailyShlokas()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
