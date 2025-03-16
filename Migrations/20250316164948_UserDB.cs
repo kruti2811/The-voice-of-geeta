57c7b7aaf142fad4990f8a163fdf3b3b4ecace8b
Er-KrutiPatel <patelkrutisubhash@gmail.com>
2025-03-16 22:23:42 +0530

    database create with migration

---
+++ Migrations/20250316164948_UserDB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace The_voice_of_geeta.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "login",
+                columns: table => new
+                {
+                    User_id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_login", x => x.User_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "login");
+        }
+    }
+}
---
+++ Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+namespace The_voice_of_geeta.Models
+{
+    public class LoginModel
+    {
+        [Key]
+        public Guid User_id { get; set; } //unique id provide
+        [Required,NotNull]
+        public string Username { get; set; }
+        [Required,NotNull]
+        public string Password { get; set; }
+
+    }
+}
