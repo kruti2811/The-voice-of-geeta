09f0c4e7eaf765b604e6c69d303e9d13d3db00f0
Dimpal karena <dimpalkarena2@gmail.com>
2025-03-27 12:47:32 +0530

    adyay pages

--- Controllers/AdhyayController.cs
+++ Controllers/AdhyayController.cs
@@ -2,11 +2,16 @@
 
 namespace The_voice_of_geeta.Controllers
 {
-    public class ShlokaController : Controller
+    public class AdhyayController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult Shloka()
+        {
+            return View();
+        }
+
     }
 }
